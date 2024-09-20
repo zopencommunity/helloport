525704f75671e9ed0a0c9c504df7bb0497e69731
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:15 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/helloport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/helloport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/helloport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Hello program produces a familiar, friendly greeting.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/helloport.git'), string(name: 'PORT_DESCRIPTION', value: 'The GNU Hello program produces a familiar, friendly greeting.' )]
   }
 }
